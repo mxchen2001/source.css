f966c32932e306cecdf8ca8a33a30dc83def46b8
Xige <mxchen2001@gmail.com>
2024-01-13 11:50:00 -0800

    update

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function copyToClipboard(id) {
     var copyText = document.getElementById(id);
     navigator.clipboard.writeText(copyText.innerText);
 }
+
+function toggleCodeBlock(id) {
+    // var copyText = document.getElementById(id);
+    var codeBlock = document.getElementById(id);
+    // toggle the class 'collapsed' on the code block
+    codeBlock.classList.toggle('collapsed');
+    
+}
